e6f8c9733e998216985a7caa3c97a45705f9db94
mihaiiro <mihaiiro@users.noreply.github.com>
2017-03-10 11:52:19 +0200

    Update javascript_json_from_array.js

--- javascript_json_from_array.js
+++ javascript_json_from_array.js
@@ -1,4 +1,3 @@
-// More details here: http://www.wpriders.com/snippets/javascript-json-form-array/
 
 // Make JSON from JS Array
 function arrayToJson (js_array_given) {
@@ -12,3 +11,12 @@ random_array['something'] = 'Some value';
 random_array['something_else'] = 'derp';
 
 console.log(arrayToJson(random_array)); // You should see a JSON output in your console.
+
+/*
+This snippet is part of the article published here:
+http://www.wpriders.com/snippets/javascript-json-form-array/
+
+In case you want to work with us, please post your task here:
+www.wpriders.com/hire-us-ref
+
+*/
