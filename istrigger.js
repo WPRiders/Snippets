b63c8be832a73d4568aafa6823fd3f1f64825ad6
mihaiiro <mihaiiro@users.noreply.github.com>
2017-03-10 12:02:24 +0200

    Create istrigger.js

---
+++ istrigger.js
@@ -0,0 +1,16 @@
+$(document).on('click tap', '.someElementClass', function (e) {
+    if (typeof e.isTrigger === 'undefined') {
+      console.log('Hooman did the click');
+    }else{
+      // This was triggered by 
+    }
+}
+
+/*
+This snippet is part of the article published here:
+http://www.wpriders.com/snippets/jquery-determine…y-jquery-trigger/
+
+In case you want to work with us, please post your task here:
+www.wpriders.com/hire-us-ref
+
+*/
