fac312a52ffd7d544a9a723be5f7b8b9f7f9c88d
mihaiiro <mihaiiro@users.noreply.github.com>
2017-03-17 11:16:12 +0200

    Create wpr_remote_check.php

---
+++ wpr_remote_check.php
@@ -0,0 +1,44 @@
+<?php
+
+/* *
+ * Remote check file existence
+ *
+ * Checking the file remotely without downloading it,
+ * the HEAD request is faster in this case.
+ *
+ * @param string $remote_file_path remote file address, an valid URL
+ * @return bool true means the file has been found
+ */
+function wpr_remote_check_file( $remote_file_path ) {
+	if ( ! filter_var( $remote_file_path, FILTER_VALIDATE_URL ) === false ) {
+		stream_context_set_default(
+			array(
+				'http' => array(
+					'method' => 'HEAD'
+				)
+			)
+		);
+		$headers    = get_headers( $remote_file_path, 1 );
+		$file_found = stristr( $headers[0], '200' ); // Returns all of haystack starting from and including the first occurrence of needle to the end. In our case "200 OK" If everything is ok.
+		if ( $file_found ) {
+			return true;
+		}
+
+		return false;
+	} else {
+		return false;
+	}
+}
+
+// Example of use
+$remote_image =  wpr_remote_check_file( 'http://www.wpriders.com/wp-content/uploads/2015/11/wpriders-logo@2x.png' );
+var_dump($remote_image);
+
+
+
+/*
+This snippet is part of the article published here:
+http://www.wpriders.com/uncategorized/php-remote-file-existence-check/
+In case you want to work with us, please post your task here:
+www.wpriders.com/hire-us-ref
+*/
